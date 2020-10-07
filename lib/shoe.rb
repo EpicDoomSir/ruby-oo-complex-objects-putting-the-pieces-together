a6e6ae35f02711e1a2c6370c906213f3854465cf
EpicDoomSir <eitands@gmail.com>
2020-10-06 23:26:50 -0400

    cleaned up attr calls

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,5 @@
 class Shoe
-    attr_accessor :brand
-    attr_accessor :color
-    attr_accessor :size
-    attr_accessor :material
-    attr_accessor :condition
+    attr_accessor :brand, :color, :size, :material, :condition
 
     def initialize(brand)
         @brand = brand
